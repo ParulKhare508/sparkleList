6e1814152a480b0a85840c56d8b52514971b2607
ParulKhare508 <parulk508@gmail.com>
2025-07-10 17:25:25 +0530

    updates done

--- mood_app.py
+++ mood_app.py
@@ -6,13 +6,13 @@ moods = ["Happy", "Sad", "Relaxed", "Energetic", "Romantic"]
 
 # ✅ Use EMBED links here
 playlists = {
+    "ashiqui2": "https://open.spotify.com/embed/album/0Rkv5iqjF2uenfL0OVB8hg",
     "Happy": "https://open.spotify.com/embed/playlist/3iDiMSVkGIXm8yCWcIomdY",
     "Sad": "https://open.spotify.com/embed/playlist/5QMgdPH8QvUOxcmX4bVs5d",
     "Relaxed": "https://open.spotify.com/embed/playlist/4m2R6xXf67zlHqf5kdT0mf",
     "Energetic": "https://open.spotify.com/embed/playlist/2iwCRchSeIocvoUcyQ77l7",
     "Romantic": "https://open.spotify.com/embed/playlist/0G8PGDiBwv8k8GCJHXSMzS",
     "ashiqui2": "https://open.spotify.com/embed/album/0Rkv5iqjF2uenfL0OVB8hg",
-    "ashiqui2": "https://open.spotify.com/embed/album/0Rkv5iqjF2uenfL0OVB8hg"
 }
 
 mood = st.selectbox("How are you feeling today?", moods)
