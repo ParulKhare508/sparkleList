21fe3fe548cf039f415344d538a899aa34699665
ParulKhare508 <parulk508@gmail.com>
2025-07-10 17:23:05 +0530

    removed comment

--- mood_app.py
+++ mood_app.py
@@ -1,18 +1,3 @@
-# import streamlit as st
-# st.title("🎧 Mood Playlist App")
-# moods = ["Happy", "Sad", "Relaxed", "Energetic", "Romantic"]
-# playlists = {
-#     "Happy": "https://open.spotify.com/playlist/3iDiMSVkGIXm8yCWcIomdY?si=-cn3w1NgTtaT5aWSF1Qdlg&pi=6PF9nj9pSXOxA",
-#     "Sad": "https://open.spotify.com/playlist/5QMgdPH8QvUOxcmX4bVs5d?si=tDJNF_qOT9GE93CO-bLGxw&pi=bQix83QLQtuQ1",
-#     "Relaxed": "https://open.spotify.com/playlist/4m2R6xXf67zlHqf5kdT0mf?si=CaMMcN8aSMeePImky6-eKw",
-#     "Energetic": "https://open.spotify.com/playlist/2iwCRchSeIocvoUcyQ77l7?si=67QWlKFPQnG2IQ-JClDGBQ",
-#     "Romantic": "https://open.spotify.com/playlist/0G8PGDiBwv8k8GCJHXSMzS?si=yi-FXevGQ6mgb5yxIO0Q2Q"
-# }
-# mood = st.selectbox("How are you feeling today?", moods)
-# if mood:
-#     st.subheader(f"Playlist for your mood: {mood}")
-#     st.components.v1.iframe(playlists[mood], height=400)
-
 import streamlit as st
 
 st.title("🎧 Mood Playlist App")
